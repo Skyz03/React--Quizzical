dd0e869de70e30a35ef2c039bc7402bdba4f8f58
Skyz03 <aakibdiscoo@gmail.com>
2022-04-05 10:12:32 +0545

    Fetch in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.scss";
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-    </div>
-  );
+  const [starWarsData, setStarWarsData] = React.useState({});
+  let showChar;
+
+  fetch("https://opentdb.com/api.php?amount=10&category=18&type=multiple")
+    .then((res) => res.json())
+    .then((data) => showChar(data.results));
+
+  console.log(showChar());
+
+  return <div className="App">{showChar}</div>;
 }
 
 export default App;
